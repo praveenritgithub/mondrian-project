aba8e9e6cc663069d26c0540ea23dea297fb4616
praveenritgithub <142033331+praveenritgithub@users.noreply.github.com>
2025-06-03 15:38:09 +0530

    Add files via upload

---
+++ EmployeeAttendanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.employee_attendance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAttendanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAttendanceTrackerApplication.class, args);
+	}
+
+}
